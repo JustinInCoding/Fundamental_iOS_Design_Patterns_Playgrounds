83bb578e532fbc1acae5abd37c60fc2c79863596
JustinSheng <justin.wang4@icloud.com>
2024-04-01 10:33:22 +0800

    Delegation

--- FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Delegation.xcplaygroundpage/Contents.swift
+++ FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Delegation.xcplaygroundpage/Contents.swift
@@ -11,3 +11,45 @@
  
  ## Code Example
  */
+import UIKit
+
+// Using 'class' keyword to define a class-constrained protocol is deprecated; use 'AnyObject' instead
+public protocol MenuControllerDelegate: AnyObject {
+	func menuViewController(_ menuViewController: MenuViewController, didSelectItemAtIndex index: Int)
+}
+
+public class MenuViewController: UIViewController {
+	
+	public weak var delegate: MenuControllerDelegate?
+	
+	// 1
+	@IBOutlet public var tableView: UITableView! {
+		didSet {
+			tableView.delegate = self
+			tableView.dataSource = self
+		}
+	}
+	
+	// 2
+	private let items = ["Item 1", "Item 2", "Item 3"]
+}
+
+// MARK: - UITableViewDataSource
+extension MenuViewController: UITableViewDataSource {
+	public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		cell.textLabel?.text = items[indexPath.row]
+		return cell
+	}
+	
+	public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return items.count
+	}
+}
+
+// MARK: - UITableViewDelegate
+extension MenuViewController: UITableViewDelegate {
+	public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		delegate?.menuViewController(self, didSelectItemAtIndex: indexPath.row)
+	}
+}
